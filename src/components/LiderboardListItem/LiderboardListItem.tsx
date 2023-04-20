cd37ce28cfb9b24c178fc3d53c46965ee1ab856e
Al3ksandr3 <aleksmanu777@gmail.com>
2023-04-20 21:39:08 +0400

    fix issue with colors naming on :root element

---
+++ src/components/LiderboardListItem/LiderboardListItem.tsx
@@ -0,0 +1,9 @@
+import "./LiderboardListItem.scss";
+
+// ------ COMPONENT: START ------ //
+
+export default function LiderboardListItem() {
+  return <li className="lidebboard-list-item"></li>;
+}
+
+// ------ COMPONENT: END ------ //
